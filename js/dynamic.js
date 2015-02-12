36659fe687c359e59c3c37f725bfa44b59dcdcfd
Lisa Park <lpark@constantcontact.com>
2015-02-12 15:27:33 -0500

    Added dynamic loading elements using jquery. make links look like buttons. Change style of header

--- js/dynamic.js
+++ js/dynamic.js
@@ -3,9 +3,9 @@ function showCatName(){
 }
 
 var data = ""
-
+var offset = Math.random() * 100;
 $.ajax({
-  url: "https://montanaflynn-cat-overflow.p.mashape.com/?limit=9&offset=1",
+  url: "https://montanaflynn-cat-overflow.p.mashape.com/?limit=9&offset=" + offset,
   headers: {
   	'X-Mashape-Key': 'tF8OhNKhTKmshvP35EC7XIn3VD6dp15x49WjsnW1DpioGWoPQA',
   	'Content-Type': 'text/plain'
@@ -13,13 +13,14 @@ $.ajax({
   method: 'GET',
   success: function(response){
   	data = response;
-  	console.log(response);
-  	var cat_images = data.split('\n');	
-	cat_images.forEach(parseCatImages)
+  	var cat_images = data.split('\n');
+	  cat_images.forEach(parseCatImages)
   }
 });
 
 function parseCatImages(cat_image, index, cat_images){
-	console.log(cat_image)
-	$("#image_" + index).attr("src", String(cat_image));
+  console.log(cat_image)
+  if (cat_image) {$('.grid').append("<div><img src='"+String(cat_image)+"'></img></div>")}
+	//$("#image_" + index).attr("src", String(cat_image));
 }
+
